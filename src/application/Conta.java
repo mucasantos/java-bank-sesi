f491c9569037e2b46c930206e76b3f636387464d
Samuel Santos <mucasantos@gmail.com>
2023-10-03 20:50:15 -0300

    atividade 03-10 pensionato

--- src/application/Conta.java
+++ src/application/Conta.java
@@ -3,27 +3,91 @@ package application;
 public class Conta {
 	// Atributos da conta
 	// mudando o modo de visualização
-
-	int numero;
-	String titular;
-	int agencia;
-	double saldo;
-	double limite;
-	String dataDeAbertura;
+	private int numero;
+	private String titular;
+	private int agencia;
+	private double saldo;
+	private double limite;
+	private String dataDeAbertura;
 	
-	boolean sacar(double quantidade) {		
+	public Conta() {
+		
+	}
+	
+	public Conta(int numero, String titular, int agencia, double saldo, double limite, String dataDeAbertura) {
+		this.numero = numero;
+		this.titular = titular;
+		this.agencia = agencia;
+		this.saldo = saldo;
+		this.limite = limite;
+		this.dataDeAbertura = dataDeAbertura;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
+	public String getDataDeAbertura() {
+		return dataDeAbertura;
+	}
+
+	public void setDataDeAbertura(String dataDeAbertura) {
+		this.dataDeAbertura = dataDeAbertura;
+	}
+
+	boolean sacar(double quantidade) {
+		//Verificação se tem saldo suficiente 
 		if (this.saldo < quantidade) {
 			return false;
 		} else {
-			this.saldo -= quantidade;
+			this.saldo -= quantidade; // o mesmo que this.saldo = this.saldo - quantidade
 			return true;
 		}
 	}
-	
+
 	void depositar(double quantidade) {
-		this.saldo += quantidade;
+		this.saldo += quantidade;// o mesmo que this.saldo = this.saldo + quantidade
 	}
+
+	// Retornar um valor (boolean)
 	boolean transferePara(Conta destino, double valor) {
+
 		// Verifica se temos saldo disponível
 		if (this.saldo > valor) {
 			// Atualizar o saldo, fazendo o saque
@@ -33,6 +97,14 @@ public class Conta {
 			return true;
 		} else {
 			return false;
-		}
+		}	
+	}
+
+	String recuperaDadosParaImpressao() {
+		String dados = "\nTitular: " + this.titular;
+		dados += "\nAbertura: " + this.dataDeAbertura;
+		dados += "\nSaldo: " + this.saldo;
+		dados += "\nLimite: " + this.limite;
+		return dados;
 	}
 }
--- src/application/Programa.java
+++ src/application/Programa.java
@@ -5,6 +5,37 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Conta minhaConta = new Conta(1234, "Palhaço do Mal", 245, 3500, 1000, "10/10/1910");
+		Conta contaAmigo = new Conta(1235, "Palhaço do Bem", 245, 6900, 2000, "10/10/1900");
+		
 
+
+		//Teste Transferir
+		if (contaAmigo.transferePara(minhaConta, 210.00)) {
+			System.out.println("Operação efetuada com sucesso!!");
+		} else {
+			System.out.println("Erro ao tranferir...Verifique o saldo...!!");
+		}
+
+		System.out.println("Saldo atual: " + minhaConta.getSaldo());
+
+		// TODO: Adicionar mensagem caso o cliente não consiga sacar ou uma
+		// mensagem de sucesso!
+
+		//Teste sacar
+		if (minhaConta.sacar(201.00)) {
+			System.out.println("Operação efetuada com sucesso!!");
+		} else {
+			System.out.println("Erro...Verifique o saldo...!!");
+		}
+
+		System.out.println("Saldo depois de sacar: " + minhaConta.getSaldo());
+
+		minhaConta.depositar(1234.12);
+
+		System.out.println("Saldo depois de depositar: " + minhaConta.getSaldo());
+		System.out.println(minhaConta.recuperaDadosParaImpressao());
+		System.out.println(contaAmigo.recuperaDadosParaImpressao());
+
+	}
 }
